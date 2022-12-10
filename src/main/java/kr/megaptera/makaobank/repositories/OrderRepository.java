bd0709a0f6cd9d56ed1980beee976f2fd17f1b18
jennajeh <jennajuneh@gmail.com>
2022-12-10 21:20:26 +0900

    Backdoor api 설계 재수정

--- src/main/java/kr/megaptera/makaobank/repositories/OrderRepository.java
+++ src/main/java/kr/megaptera/makaobank/repositories/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@SuppressWarnings("unchecked")
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findAllByUserId(Long userId, Pageable pageable);
 
